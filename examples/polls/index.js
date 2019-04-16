497645bbd030604c3b2b94bdfadbf3bcaa52717c
Damien BUTY <dam.buty@gmail.com>
2019-04-16 09:09:45 +0200

    Update index.js

--- examples/polls/index.js
+++ examples/polls/index.js
@@ -15,14 +15,10 @@ aido.configure({
     // If you already have a tunnel setup just uncomment the following line and enter your actual tunnel URL
     custom: 'https://aido-test.ngrok.io',
   },
-  // appId: 'AHV55743S',
-  // slackVerificationToken: '6wxydihpt0AnuG7cB39gMXkl',
-  // botToken: 'xoxp-284476586484-284435374338-607434076736-0db6220bfa82a89bf044f2e089407905',
-  // legacyToken: 'xoxp-284476586484-284435374338-595973987139-f948ddc1f0f45404a54e19566d87b9d8',
-  appId: 'ADFGTV25C',
-  slackVerificationToken: 'PE1gvdLlApVrdEKB4YUeaP1X',
-  botToken: 'xoxp-26655536611-168801474546-455596679361-25383179482bb86765251cbc3dbfd8ce',
-  legacyToken: 'xoxp-26655536611-168801474546-444605398785-ebd4e874791389e49bdba65681035ec2',
+  // appId: 'AXXXLOLOLOL',
+  // slackVerificationToken: 'xxxxxxxxxxxxxxxxx',
+  // botToken: 'xoxp-xxxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxx',
+  // legacyToken: 'xoxp-xxxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxx',
   // Use the following option if you don't want aido to check that slash commands are installed on your workspace
   // noCommandIntrospection: true,
 })
