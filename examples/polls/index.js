d0a080c69eb871dfe98ffc55f5274e5d2b69d409
Damien BUTY <dam.buty@gmail.com>
2019-09-05 17:22:23 +0200

    ✏️ Remove Slack tokens

--- examples/polls/index.js
+++ examples/polls/index.js
@@ -7,15 +7,17 @@ aido.init({
   getSlackProfile: true,
   slash: { polack: Polack },
   plugins: [pollsPlugin],
-  tunnel: {
-    // If you already have a tunnel setup just uncomment the following line and enter your actual tunnel URL
-    custom: 'https://aido-test.ngrok.io',
-  },
-  appId: 'ADFGTV25C',
-  slackVerificationToken: 'l6QWLDSRgbWYO7oqG28t7yFh',
-  appToken: 'xoxp-26655536611-168801474546-743125131991-1f728d2158b04ad368151fb01bd10ff8',
-  botToken: 'xoxb-236786829587-4jE6wkRi4UuuJTVku8wxcOc1',
-  legacyToken: 'xoxp-26655536611-168801474546-741269345328-ad0ca072c1cabb847b894d0eaa9dc077',
+  // tunnel: {
+  //   // If you already have a tunnel setup just uncomment the following line and enter your actual tunnel URL
+  //   custom: 'https://xxxxxx.ngrok.io',
+  //   // Or you can let aido create one on localtunnel.me for you (and request a custom subdomain !)
+  //   lt: { subDomain: 'xxxxxx' },
+  // },
+  // appId: 'AXXXXXXX',
+  // slackVerificationToken: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+  // appToken: 'xoxp-xxxxxxxxx-xxxxxxxx',
+  // botToken: 'xoxb-xxxxxxxxx',
+  // legacyToken: 'xoxp-xxxxxxxxx-xxxxxxxx',
 })
 
 aido.start(3000)
