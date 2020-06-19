f2d924d4ab3af88b3beb9fbbca4bad467291b6d7
Damien Buty <damien@comet.co>
2020-06-19 11:09:35 +0200

    📦 Prune dependencies

--- examples/polls/index.js
+++ examples/polls/index.js
@@ -10,8 +10,6 @@ aido.init({
   // tunnel: {
   //   // If you already have a tunnel setup just uncomment the following line and enter your actual tunnel URL
   //   custom: 'https://xxxxxx.ngrok.io',
-  //   // Or you can let aido create one on localtunnel.me for you (and request a custom subdomain !)
-  //   lt: { subDomain: 'xxxxxx' },
   // },
   // appId: 'AXXXXXXX',
   // slackVerificationToken: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
